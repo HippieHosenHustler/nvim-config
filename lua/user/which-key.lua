2f07537f395460151da9d14749e5b9c8a9697d17
HippieHosenHustler <scharfe.edwin@gmail.com>
2022-09-22 18:55:47 +0200

    configure keybindings for lazygit

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -10,4 +10,11 @@ wk.register({
   e = { "<cmd>NvimTreeToggle<cr>", "Open File Tree" },
   f = { "<cmd>Telescope find_files<cr>", "File Search" },
   t = { "<cmd>Telescope live_grep", "Fulltext Search" },
+  g = {
+    name = "Git",
+    g = { "<cmd>LazyGit<cr>", "Open Lazygit" },
+    x = { "<cmd>LazyGitConfig<cr>", "Configure Lazygit" },
+    c = { "<cmd>LazyGitFilter<cr>", "View Commits" },
+    f = { "<cmd>LazyGitFilterCurrentFile<cr>", "View Commits for current file" }
+  }
 }, { prefix = "<leader>" })

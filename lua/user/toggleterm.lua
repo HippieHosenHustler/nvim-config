d0410e90f266ed3e8e36becfb04c5309c3076186
HippieHosenHustler <scharfe.edwin@gmail.com>
2022-11-02 22:53:01 +0100

    stuff

---
+++ lua/user/toggleterm.lua
@@ -0,0 +1,9 @@
+
+local status_ok, tt = pcall(require, "toggleterm")
+if not status_ok then
+  return
+end
+
+tt.setup {
+
+}
--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -9,12 +9,17 @@ wk.setup {
 wk.register({
   e = { "<cmd>NvimTreeToggle<cr>", "Open File Tree" },
   f = { "<cmd>Telescope find_files<cr>", "File Search" },
-  t = { "<cmd>Telescope live_grep", "Fulltext Search" },
+  s = { "<cmd>Telescope live_grep<cr>", "Fulltext Search" },
   g = {
     name = "Git",
     g = { "<cmd>LazyGit<cr>", "Open Lazygit" },
     x = { "<cmd>LazyGitConfig<cr>", "Configure Lazygit" },
     c = { "<cmd>LazyGitFilter<cr>", "View Commits" },
     f = { "<cmd>LazyGitFilterCurrentFile<cr>", "View Commits for current file" }
+  },
+  t = {
+    name = "Terminal",
+    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+    t = { "<cmd>ToggleTerm direction=tab<cr>", "Tab" }
   }
 }, { prefix = "<leader>" })
